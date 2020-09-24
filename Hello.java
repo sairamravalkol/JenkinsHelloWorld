2c410d83235f187f633a04f4b0f9e6428347ce8f
R SAIRAM <sairam.ravulakolu@gmail.com>
2020-09-24 14:12:08 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 public static void main(String...args) {
 
 System.out.println("Hello World This is for Remote");
+  System.out.println("This is second line from github.com");
 }
 }
